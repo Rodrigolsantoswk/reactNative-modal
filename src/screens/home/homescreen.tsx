ee3a549150d95c0bc1cc2393530c48b8d19887af
rodrigolsantoswk <limasrodrigowk@gmail.com>
2025-03-08 22:49:14 -0300

    aplicativo atualizado

--- src/screens/home/homescreen.tsx
+++ src/screens/home/homescreen.tsx
@@ -1,67 +1,99 @@
 import { Button } from "@/src/components/Button";
 import { ImageViewer } from "@/src/components/ImageViewer";
 import { ThemedView } from "@/src/components/ThemedView";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 import { router } from "expo-router";
 import ModalScreen from "@/src/components/modalscreen";
 import { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form";
 import { ThemedText } from "@/src/components/ThemedText";
+import { v4 as uuidv4 } from "uuid"; // Importa o gerador de UUID
 
-interface IFormInput {
-    email: string
+interface IFormulario {
+  email: string;
 }
 
 const PlaceholderImage = require("@/assets/images/zero-um-bw.png");
+
 export function HomeScreen() {
-    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-    const onModalClose = () => {
-        setIsModalVisible(false);
-    };
-    const clickDetails = () => {
-        router.navigate("/details")
-    }
-    // react hook form
-    const { register, handleSubmit } = useForm<IFormInput>()
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
-   
-    // react hook form
-    return (
-        <ThemedView style={styles.container}>
-            <ThemedView style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceholderImage} />
-            </ThemedView>
-            <ThemedView style={styles.footerContainer}>
-                <Button theme="primary" label="Modal" onPress=
-                {()=>setIsModalVisible(true)} />
-                <Button label="Details" onPress={clickDetails} />
-            </ThemedView>
-            <ModalScreen isVisible={isModalVisible} onClose={onModalClose}>
-            {
-                <ThemedView>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register("email", { required: true, maxLength: 20 })} />
-                        <input type="submit" />
-                    </form>
-                    <ThemedText></ThemedText>
-                </ThemedView>
-                
-            }
-            </ModalScreen>
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [uuid, setUuid] = useState<string>();
+  const { handleSubmit, setValue } = useForm<IFormulario>();
+
+  const clickDetails = () => {
+    router.navigate("/details");
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+    setUuid(""); // caso o modal seja fechado, o UID retorna para vazio limpando o texto na tela.
+  };
+
+  const onSubmit: SubmitHandler<IFormulario> = (data) => {
+    let regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    
+    if (regEmail.test(data.email)) return setUuid(uuidv4());
+    alert("Email inválido"); // caso o return não seja acionado, então o alert informará que o email é inválido.
+  };
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </ThemedView>
+      <ThemedView style={styles.footerContainer}>
+        <Button theme="primary" label="Modal" onPress={() => setIsModalVisible(true)} />
+        <Button label="Details" onPress={clickDetails} />
+      </ThemedView>
+
+      <ModalScreen isVisible={isModalVisible} onClose={onModalClose}>
+        <ThemedView style={styles.modalContent}>
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={styles.input}
+              onChangeText={(text) => setValue("email", text)}
+            />
+            <Button theme="primary" label="enviar" onPress={handleSubmit(onSubmit)}/>
+            {uuid && ( // Caso a variável não possuia atribuição, o texto não aparece. 
+                       // https://codeburst.io/javascript-short-circuit-conditionals-bbc13ac3e9eb
+                <ThemedText style={styles.uuidText}>
+                    UUID Gerado: {uuid}
+                </ThemedText>
+            )}
         </ThemedView>
-    );
+      </ModalScreen>
+    </ThemedView>
+  );
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    imageContainer: {
-        flex: 1,
-    },
-    footerContainer: {
-        flex: 1 / 3,
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  modalContent: {
+    padding: 20,
+    alignItems: "center",
+  },
+  input: {
+    width: 250,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    color: "white",
+  },
+  uuidText: {
+    marginTop: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
 });
